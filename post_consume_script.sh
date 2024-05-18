77f0c8cc0beb5e4609b02046e50b1b70be467066
Felix Eckhofer <felix@eckhofer.com>
2024-05-18 11:13:09 +0200

    Quote RUN_DIR to prevent word splitting
    Also makes the scripts more robust by enabling bash options to terminate if anything goes wrong.

--- post_consume_script.sh
+++ post_consume_script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-RUN_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
-source $RUN_DIR/venv/bin/activate
-$RUN_DIR/post_consume_script.py
+set -eEuo pipefail
+
+RUN_DIR="$(dirname -- "$(readlink -f -- "$0")")"
+source "$RUN_DIR/venv/bin/activate"
+exec "$RUN_DIR/post_consume_script.py"
--- setup_venv.sh
+++ setup_venv.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-RUN_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
+set -eEuo pipefail
+
+RUN_DIR="$(dirname -- "$(readlink -f -- "$0")")"
 echo -n "Setting up virtual environment..."
-python -m venv --system-site-packages $RUN_DIR/venv
+python -m venv --system-site-packages "$RUN_DIR/venv"
 echo "OK"
-source $RUN_DIR/venv/bin/activate
-pip install -r $RUN_DIR/requirements.txt
+source "$RUN_DIR/venv/bin/activate"
+pip install -r "$RUN_DIR/requirements.txt"
 echo -e "\npaperless-ngx-postprocessor setup successful"
